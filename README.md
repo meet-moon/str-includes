3f14f9e416559f84d6b120c4e77d698ea597d296
Meet M <>
2023-01-25 15:59:54 +0530

    now you can search any string

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # str-includes
+
+This NPM find keyword that you want to find in your string and it find as a substring of that string as well.
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function str_includes(str) {
-    return str.includes("Hello world")
+function str_includes(str, keyword) {
+    if (typeof str == "string") {
+        return str.includes(keyword)
+    } else {
+        throw "argument must be a string"
+    }
 }
 module.exports = str_includes
