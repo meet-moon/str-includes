0143fb526837ce739ee2561032fe0830852b757c
Meet M <>
2023-06-30 12:39:24 +0530

    new changes

---
+++ dist/button.js
@@ -0,0 +1,4 @@
+const Button = () => {
+  return /*#__PURE__*/React.createElement("button", null, "This is a button component");
+};
+export default Button;
--- index.js
+++
@@ -1,9 +0,0 @@
-const Component1 = () => {
-    return <div>Component 1</div>;
-};
-const Component2 = () => {
-    return <div>Component 2</div>;
-};
-
-
-export { Component1, Component2 };
