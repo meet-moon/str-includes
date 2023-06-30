692b0e7cf0030107429411361634540599f85795
Meet M <>
2023-06-30 17:00:14 +0530

    test

---
+++ dist/components/Main.js
@@ -0,0 +1,77 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+const Main = () => {
+  return /*#__PURE__*/_react.default.createElement("main", {
+    class: "py-6 px-4 sm:p-6 md:py-10 md:px-8"
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    class: "max-w-4xl mx-auto grid grid-cols-1 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2"
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    class: "relative p-3 col-start-1 row-start-1 flex flex-col-reverse rounded-lg bg-gradient-to-t from-black/75 via-black/0 sm:bg-none sm:row-start-2 sm:p-0 lg:row-start-1"
+  }, /*#__PURE__*/_react.default.createElement("h1", {
+    class: "mt-1 text-lg font-semibold text-white sm:text-slate-900 md:text-2xl dark:sm:text-white"
+  }, "Beach House in Collingwood"), /*#__PURE__*/_react.default.createElement("p", {
+    class: "text-sm leading-4 font-medium text-white sm:text-slate-500 dark:sm:text-slate-400"
+  }, "Entire house")), /*#__PURE__*/_react.default.createElement("dl", {
+    class: "mt-4 text-xs font-medium flex items-center row-start-2 sm:mt-1 sm:row-start-3 md:mt-2.5 lg:row-start-2"
+  }, /*#__PURE__*/_react.default.createElement("dt", {
+    class: "sr-only"
+  }, "Reviews"), /*#__PURE__*/_react.default.createElement("dd", {
+    class: "text-indigo-600 flex items-center dark:text-indigo-400"
+  }, /*#__PURE__*/_react.default.createElement("svg", {
+    width: "24",
+    height: "24",
+    fill: "none",
+    "aria-hidden": "true",
+    class: "mr-1 stroke-current dark:stroke-indigo-500"
+  }, /*#__PURE__*/_react.default.createElement("path", {
+    d: "m12 5 2 5h5l-4 4 2.103 5L12 16l-5.103 3L9 14l-4-4h5l2-5Z",
+    "stroke-width": "2",
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round"
+  })), /*#__PURE__*/_react.default.createElement("span", null, "4.89 ", /*#__PURE__*/_react.default.createElement("span", {
+    class: "text-slate-400 font-normal"
+  }, "(128)"))), /*#__PURE__*/_react.default.createElement("dt", {
+    class: "sr-only"
+  }, "Location"), /*#__PURE__*/_react.default.createElement("dd", {
+    class: "flex items-center"
+  }, /*#__PURE__*/_react.default.createElement("svg", {
+    width: "2",
+    height: "2",
+    "aria-hidden": "true",
+    fill: "currentColor",
+    class: "mx-3 text-slate-300"
+  }, /*#__PURE__*/_react.default.createElement("circle", {
+    cx: "1",
+    cy: "1",
+    r: "1"
+  })), /*#__PURE__*/_react.default.createElement("svg", {
+    width: "24",
+    height: "24",
+    fill: "none",
+    stroke: "currentColor",
+    "stroke-width": "2",
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
+    class: "mr-1 text-slate-400 dark:text-slate-500",
+    "aria-hidden": "true"
+  }, /*#__PURE__*/_react.default.createElement("path", {
+    d: "M18 11.034C18 14.897 12 19 12 19s-6-4.103-6-7.966C6 7.655 8.819 5 12 5s6 2.655 6 6.034Z"
+  }), /*#__PURE__*/_react.default.createElement("path", {
+    d: "M14 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
+  })), "Collingwood, Ontario")), /*#__PURE__*/_react.default.createElement("div", {
+    class: "mt-4 col-start-1 row-start-3 self-center sm:mt-0 sm:col-start-2 sm:row-start-2 sm:row-span-2 lg:mt-6 lg:col-start-1 lg:row-start-3 lg:row-end-4"
+  }, /*#__PURE__*/_react.default.createElement("button", {
+    type: "button",
+    class: "bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg"
+  }, "Check availability")), /*#__PURE__*/_react.default.createElement("p", {
+    class: "mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400"
+  }, "This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.")));
+};
+var _default = Main;
+exports.default = _default;
--- dist/index.js
+++ dist/index.js
@@ -9,5 +9,13 @@ Object.defineProperty(exports, "Button", {
     return _button.default;
   }
 });
+Object.defineProperty(exports, "Main", {
+  enumerable: true,
+  get: function get() {
+    return _Main.default;
+  }
+});
+require("tailwindcss/tailwind.css");
 var _button = _interopRequireDefault(require("./components/button"));
+var _Main = _interopRequireDefault(require("./components/Main"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
