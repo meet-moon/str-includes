213df4cb01cce89fa2e2f168b58f57f84ba3575a
Meet M <>
2023-06-30 15:49:20 +0530

    s

--- dist/components/button.js
+++ dist/components/button.js
@@ -8,7 +8,7 @@ var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const Button = props => {
   return /*#__PURE__*/_react.default.createElement("button", {
-    className: className,
+    className: props.className,
     type: props.type,
     name: props.name,
     value: props.value
