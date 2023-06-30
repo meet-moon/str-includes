7527347ad3839755589278fda93e7c5fe00e3b4e
Meet M <>
2023-06-30 10:21:45 +0530

    new change

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ function str_includes(str) {
 }
 module.exports = str_includes
 
-function LargeComponent() {
-    return <div>This is a large component</div>
-}
-function SmallComponent() {
-    return <div>This is a small component</div>
-}
-module.exports = LargeComponent
+const Component1 = () => {
+    return <div>Component 1</div>;
+};
+const Component2 = () => {
+    return <div>Component 2</div>;
+};
 
-module.exports = SmallComponent
+
+export { Component1, Component2 };
