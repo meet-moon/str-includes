a3370557aa7f9b8d7029356aa2d8986fcf269488
Meet M <>
2023-06-30 15:45:12 +0530

    create new build

--- dist/components/button.js
+++ dist/components/button.js
@@ -8,14 +8,13 @@ var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const Button = props => {
   return /*#__PURE__*/_react.default.createElement("button", {
-    className: "btn btn--".concat(props.kind, " CTA"),
-    "data-id": props.id,
+    className: className,
     type: props.type,
     name: props.name,
     value: props.value,
     disabled: props.disabled,
     onClick: props.handleClick
-  }, /*#__PURE__*/_react.default.createElement("h4", null, props.label));
+  }, props.children);
 };
 var _default = Button;
 exports.default = _default;
